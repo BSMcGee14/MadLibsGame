7e805844e7fe46c5315189b98bf5586e63d10361
BSMcGee14 <bmcgee6@mail.csuchio.edu>
2018-09-12 17:38:22 -0700

    Madlibs program

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,26 @@
 //Authors: 
 #include <iostream>
 #include <string>
+using namespace std;
+
+string name, pronoun, misterName, noun, yourName, docName;
 
 int main()
 {
+    cout << "Please insert name: ";
+    cin >> misterName;
+    cout << "Please insert a pronoun: ";
+    cin >> pronoun;
+    cout << "Please insert another pronoun: ";
+    cin >> noun;
+    cout << "Please insert another name: ";
+    cin >> docName;
+    cout << "Please insert one more name: ";
+    cin >> yourName;
+    cout << "I'm terribly sorry Mr. " << misterName << ", I'm not gonna be able to come to work today. \n";
+    cout << "Last night I had gone to " << pronoun << " and had broken my " << noun << " while partying. \n";
+    cout << "I'm going to the hospital to see doctor " << docName  << "to wrap up my broken bone. I will see you, \n";
+    cout << "tomorrow. Sincerely, " << yourName;
 
   return 0;
 }
